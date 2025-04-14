83d2a63572eab2cf0c464315b088a4bbfc501f74
sasquare <femisasa@gmail.com>
2025-04-14 13:46:47 +0100

    Initial project setup with FastAPI and React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Welcome to ArtisanHub</h1>
+      <p>Connecting Nigerian artisans with customers.</p>
+    </div>
+  );
+}
+
+export default App;
